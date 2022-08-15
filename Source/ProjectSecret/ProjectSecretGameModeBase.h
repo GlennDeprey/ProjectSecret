035ad4d7e12d19d3e6789f3e209ee75a645d526a
GlennDeprey <glenndepreydae@gmail.com>
2022-08-15 21:32:35 +0200

    SecretV0.1

---
+++ Source/ProjectSecret/ProjectSecretGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectSecretGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSECRET_API AProjectSecretGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
