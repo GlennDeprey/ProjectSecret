72b7dc01173b22b5f8272584df19a525acd12268
GlennDeprey <glenndepreydae@gmail.com>
2022-08-19 20:26:10 +0200

    SecretV0.1

--- Source/ProjectSecret/Private/BaseModules/SP_CharacterBase.cpp
+++ Source/ProjectSecret/Private/BaseModules/SP_CharacterBase.cpp
@@ -2,6 +2,7 @@
 
 
 #include "BaseModules/SP_CharacterBase.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 ASP_CharacterBase::ASP_CharacterBase()
@@ -9,13 +10,124 @@ ASP_CharacterBase::ASP_CharacterBase()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Setup Spring Arm Component
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmComp->bUsePawnControlRotation = true;
+	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->SetRelativeLocation(FVector {20.f, 0.f, 70.f});
+	SpringArmComp->ProbeSize = 5.f;
+	SpringArmComp->CameraLagSpeed = 20.f;
+	SpringArmComp->CameraRotationLagSpeed = 0.f;
+	SpringArmComp->bEnableCameraLag = true;
+	SpringArmComp->bInheritRoll = false;
+	SpringArmComp->bUsePawnControlRotation = true;
+
+
+	// Setup Camera Component
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComp->SetupAttachment(SpringArmComp);
+	CameraComp->SetRelativeScale3D_Direct(FVector{0.25f, 0.25f, 0.25f});
+	CameraComp->bUsePawnControlRotation = false;
+
+
+	// Set Orient Rotation to Movement enabled
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	// Disable Controller Rotation Yaw
+	bUseControllerRotationYaw = false;
 }
 
 // Called when the game starts or when spawned
 void ASP_CharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// Camera Zoom Setup
+	if (CameraZoomCurve)
+	{
+		// Zoom in Timeline Float 
+		FOnTimelineFloat CameraZoomInTimelineFloat;
+		CameraZoomInTimelineFloat.BindUFunction(this, FName("CameraZoomInTimelineF"));
+		CameraZoomInTimeline.AddInterpFloat(CameraZoomCurve, CameraZoomInTimelineFloat);
+		CameraZoomInTimeline.SetLooping(false);
+		
+		// Zoom out Timeline Float 
+		FOnTimelineFloat CameraZoomOutTimelineFloat;
+		CameraZoomOutTimelineFloat.BindUFunction(this, FName("CameraZoomOutTimelineF"));
+		CameraZoomOutTimeline.AddInterpFloat(CameraZoomCurve, CameraZoomOutTimelineFloat);
+		CameraZoomOutTimeline.SetLooping(false);
+	}
+}
+
+// Camera Zoom in Timeline Function
+void ASP_CharacterBase::CameraZoomInTimelineF()
+{
+	CameraZoomTimelineValue = CameraZoomInTimeline.GetPlaybackPosition();
+	// The float value adjusts the camera zoom force
+	CameraZoomCurveFloatValue = 5.f * CameraZoomCurve->GetFloatValue(CameraZoomTimelineValue);
+	// Updates the previous location of the camera
+	const FVector NewCameraLocation = FVector{CameraComp->GetRelativeLocation().X + CameraZoomCurveFloatValue, CameraComp->GetRelativeLocation().Y, CameraComp->GetRelativeLocation().Z};
 	
+	CameraComp->SetRelativeLocation(NewCameraLocation);
+}
+
+// Zooms the camera in and checks if the location is not out of bounds
+void ASP_CharacterBase::CameraZoomIn()
+{
+	// Max Zoom in
+	if (CameraComp->GetRelativeLocation().X >= 150.f) return;
+	
+	CameraZoomInTimeline.PlayFromStart();
+}
+
+// Camera Zoom out Timeline Function
+void ASP_CharacterBase::CameraZoomOutTimelineF()
+{
+	CameraZoomTimelineValue = CameraZoomOutTimeline.GetPlaybackPosition();
+	// The float value adjusts the camera zoom force
+	CameraZoomCurveFloatValue = -5.f * CameraZoomCurve->GetFloatValue(CameraZoomTimelineValue);
+	// Updates the previous location of the camera
+	const FVector NewCameraLocation = FVector{CameraComp->GetRelativeLocation().X + CameraZoomCurveFloatValue, CameraComp->GetRelativeLocation().Y, CameraComp->GetRelativeLocation().Z};
+	
+	CameraComp->SetRelativeLocation(NewCameraLocation);
+}
+
+// Zooms the camera out and checks if the location is not out of bounds
+void ASP_CharacterBase::CameraZoomOut()
+{
+	// Max Zoom out
+	if (CameraComp->GetRelativeLocation().X <= -150.f) return;
+	
+	CameraZoomOutTimeline.PlayFromStart();
+}
+
+void ASP_CharacterBase::CameraZoomReset()
+{
+	CameraComp->SetRelativeLocation(FVector{0.f, CameraComp->GetRelativeLocation().Y, CameraComp->GetRelativeLocation().Z});
+}
+
+// Move forwards and backwards
+void ASP_CharacterBase::MoveForward(float Value)
+{
+	// Get control rotation, and zeros out pitch and roll in order to avoid funky stuff
+	FRotator ControlRot = GetControlRotation();
+	ControlRot.Pitch = 0.0f;
+	ControlRot.Roll = 0.0f;
+	AddMovementInput(ControlRot.Vector(), Value, false);
+}
+
+// Move right and left
+void ASP_CharacterBase::MoveRight(float Value)
+{
+	// Get control rotation, and zeros out pitch and roll in order to avoid funky stuff
+	FRotator ControlRot = GetControlRotation();
+	ControlRot.Pitch = 0.0f;
+	ControlRot.Roll = 0.0f;
+
+	// Gets the Y Axis from Control rotation
+	FVector RightVector = FRotationMatrix(ControlRot).GetScaledAxis(EAxis::Y);
+	
+	AddMovementInput(RightVector, Value, false);
 }
 
 // Called every frame
@@ -23,6 +135,9 @@ void ASP_CharacterBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// Camera Zoom Timeline Tick
+	CameraZoomInTimeline.TickTimeline(DeltaTime);
+	CameraZoomOutTimeline.TickTimeline(DeltaTime);
 }
 
 // Called to bind functionality to input
@@ -30,5 +145,16 @@ void ASP_CharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASP_CharacterBase::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASP_CharacterBase::MoveRight);
+	
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("Lookup", this, &APawn::AddControllerPitchInput);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	
+	PlayerInputComponent->BindAction("CameraZoomIn", IE_Pressed, this, &ASP_CharacterBase::CameraZoomIn);
+	PlayerInputComponent->BindAction("CameraZoomOut", IE_Pressed, this, &ASP_CharacterBase::CameraZoomOut);
+	PlayerInputComponent->BindAction("CameraZoomReset", IE_Pressed, this, &ASP_CharacterBase::CameraZoomReset);
 }
 
--- Source/ProjectSecret/Public/BaseModules/SP_CharacterBase.h
+++ Source/ProjectSecret/Public/BaseModules/SP_CharacterBase.h
@@ -3,8 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/TimelineComponent.h"
 #include "SP_CharacterBase.generated.h"
+
+// Preload components in order to not have any future issues
+class UCameraComponent;
+class USpringArmComponent;
 
 UCLASS()
 class PROJECTSECRET_API ASP_CharacterBase : public ACharacter
@@ -18,6 +25,70 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	/////////////////////////////////////
+	/*             CAMERA              */
+	/////////////////////////////////////
+
+	// Spring arm positioning the camera behind the Character
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+
+	// Camera is attached to the spring arm that follows the Character
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComp;
+
+	// Curve float for Camera Zoom
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	UCurveFloat* CameraZoomCurve;
+
+	// Current Timeline value in order to have a smooth transition
+	float CameraZoomTimelineValue;
+
+	// Current Float value to multiply with the camera force in order to have a smooth transition
+	float CameraZoomCurveFloatValue;
+	
+	// Timeline for Camera Zoom In
+	UPROPERTY()
+	FTimeline CameraZoomInTimeline;
+
+	// Camera Zoom in Timeline function
+	UFUNCTION()
+	void CameraZoomInTimelineF();
+
+	// Camera Zoom in input and bound check
+	UFUNCTION()
+	void CameraZoomIn();
+
+	// Timeline for Camera Zoom Out
+	UPROPERTY()
+	FTimeline CameraZoomOutTimeline;
+
+	// Camera Zoom Out Timeline function
+	UFUNCTION()
+	void CameraZoomOutTimelineF();
+
+	// Camera Zoom Out input and bound check
+	UFUNCTION()
+	void CameraZoomOut();
+	
+	// Camera Zoom Reset input
+	UFUNCTION()
+	void CameraZoomReset();
+	
+	/***********************************/
+
+	/////////////////////////////////////
+	/*             MOVEMENT            */
+	/////////////////////////////////////
+	
+	// Move Forwards and Backwards     
+	void MoveForward(float Value);  
+
+	// Move Right and Left             
+	void MoveRight(float Value);
+
+	/***********************************/
 
 public:	
 	// Called every frame
